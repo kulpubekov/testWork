b0965dbe6c5be9f944d86c6663ce4f4182749e3c
kulpubekov <kulpubekov@gmail.com>
2020-08-12 20:56:49 +0600

    fiasko tasks ))

--- src/store.js
+++ src/store.js
@@ -6,17 +6,18 @@ const persistedState = localStorage.getItem('tasks')
   : {}
 
 
-const store = createStore(reducer, persistedState);
+const store = createStore(
+  reducer,
+  // persistedState
+);
 
 const update = () => {
 
-  localStorage.setItem('tasks', JSON.stringify(store.getState()))
+  localStorage.setItem('tasks', JSON.stringify(store.getState().taskList.tasks))
 };
 
 
-
 store.subscribe(update);
-
 
 
 export default store;
